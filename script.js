3f50403bc55b61bec07ee91dd018a0f833325721
timurzubcenko <110413600+timurzubcenko@users.noreply.github.com>
2022-09-27 21:59:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const input = document.querySelector('#input')
 const h3 = document.querySelector('h3')
 const btn = document.querySelector('button')
 const p = document.querySelector('p')
+const btns = document.querySelector('.btns')
+const btnRestart = `<div class="btn">Начать заново</div>`
 
 let number = Math.floor(Math.random() * 100)
-let tries = 0
-
 console.log(number)
 
+let tries = 0
+
+//Play
 btn.addEventListener('click', LowHight)
 input.addEventListener('keydown', clickEnter)
+
+//Restart
+btns.addEventListener('click', restart)
 
 function LowHight() {
     if (input.value > number) {
@@ -25,13 +31,17 @@ function LowHight() {
     }
     else if (Number(input.value) == number) {
         p.innerText = `Угадал!!! Загаданое число: ${number}`
+        btns.innerHTML = btnRestart
+        input.disabled = true
     }
     else if (String(input.value)) {
         p.innerText = 'Введите число!'
     }
     input.value = ""
-    if (tries === 5) {
+    if (tries >= 5) {
         p.innerText = `Проиграл!!! Загаданое число: ${number}`
+        btns.innerHTML = btnRestart
+        input.disabled = true
     }
     console.log(tries)
 }
@@ -40,4 +50,8 @@ function clickEnter(e) {
     if (e.keyCode === 13) {
         LowHight()
     }
+}
+
+function restart() {
+    location.reload()
 }
